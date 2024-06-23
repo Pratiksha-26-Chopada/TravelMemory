d638a3d53d3bf5a6022df18936f6ea3134869bd8
Pratiksha Chopada <pratikshachopada@gmail.com>
2024-06-23 11:55:25 +0530

    ADD: completed backend for travel memmory

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,16 @@
-const express = require("express")
-require('dotenv').config()
+const express = require("express");
+require('dotenv').config();
+const cors = require('cors');
 
-const app = express()
-PORT = process.env.PORT
-const conn = require('./conn')
+const app = express();
+PORT = process.env.PORT;
+const conn = require('./conn');
+const tripRoutes = require('./routes/trip.routes');
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
+
+app.use('/trip',tripRoutes);
 
 app.get('/hello' , (req,res)=>{
     res.send('Hello World!')
